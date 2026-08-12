757a6e44318a8d834b8080587efdea0fe03f05fa
Rohan <rohanprajjapati@gmail.com>
2026-08-12 16:49:20 +0530

    cloudinary feature apply

--- backend/middlewares/upload.js
+++ backend/middlewares/upload.js
@@ -1,13 +1,21 @@
 const multer = require("multer");
-const path = require("path");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("cloudinary").v2;
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, uniqueName + path.extname(file.originalname));
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "uploads", // cloudinary account me folder ka naam
+    allowed_formats: ["jpg", "png", "jpeg", "pdf", "mp4"], // apni need ke hisaab se
+    public_id: (req, file) => {
+      return Date.now() + "-" + Math.round(Math.random() * 1e9);
+    },
   },
 });
 
