0cd4fe28887557fa0d6431bffdf885cd881c40db
Rohan <rohanprajjapati@gmail.com>
2026-08-12 13:02:33 +0530

    changes

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,4 +1,4 @@
-const userSchema = require("./../models/UserSchema");
+const userSchema = require("./../models/userSchema");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
